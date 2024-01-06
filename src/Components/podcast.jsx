3ce34b2ab443ee21c492fea95fd55e92f4eb1c5b
mohabash011 <mohabashuni@gmail.com>
2024-01-06 11:41:39 +0300

    padding fixed in podcast.jsx

--- src/Components/podcast.jsx
+++ src/Components/podcast.jsx
@@ -4,25 +4,25 @@ import '../../src/App.css'
 export default function Services() {
   return (
   <div>
-<div class="bg-gray-400 flex font-29 justify-center items-center ">
-    <div style={{ direction: 'rtl' }} class="flex w-3/4 mx-auto flex-col p-4 m-2 justify-center items-center text-center bg-opacity-60 bg-gray-300 border border-gray-400 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+<div class="bg-gray-400 flex font-29 justify-center items-center  ">
+    <div style={{ direction: 'rtl' }} class="flex w-3/4 mx-auto p-8 flex-col m-2 justify-center items-center text-center bg-opacity-60 bg-gray-300 border border-gray-400 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
     <span class="flex text-center items-center mb-2 text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900 dark:text-white">
     ليش لازم تسوّي بودكاست؟ 
     </span>
         <ul style={{ direction: 'rtl' }} className='flex flex-col items-center justify-center list-disc list-inside'>
-        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl tracking-tight py-4 text-gray-900 dark:text-gray-400">
             يمكنك من الوصول لجمهور محدود العدد، لكنه بالغ التأثير؛  فحسب الإحصاءات، 90% من المستمعين يُكمِلون الحلقة كاملة!
         </li>
-        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl  py-4 text-gray-900 dark:text-gray-400">
             بوسعك إقناعه بشكل أسهل من غيره في  الإيمان بأفكارك  أو  اقتناء منتجات.
         </li>
-        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl  py-4 text-gray-900 dark:text-gray-400">
          على عكس منصات التواصل الأخرى، لا يشترط امتلاك شهادة موثوق للإعلان داخل الحلقات.
         </li>
-        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl  py-4 text-gray-900 dark:text-gray-400">
             ولست ملزمًا بالإفصاح عن الإعلان. 
         </li>
-        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+        <li class="flex text-center items-center text-xl sm:text-xl md:text-2xl  py-4 text-gray-900 dark:text-gray-400">
             وهذا يمكنك من جلب إيرادات لك بدون أي تكاليف إضافية.
         </li>
         </ul>
@@ -30,7 +30,7 @@ export default function Services() {
     </div>
 
     <div class="bg-gray-400  flex justify-center items-center font-29 ">
-    <div style={{ direction: 'rtl' }} class="flex leading-30 w-3/4 mx-auto flex-col justify-center text-center bg-gray-300 border border-gray-400 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div style={{ direction: 'rtl' }} class="flex leading-30 w-3/4 mx-auto p-8 flex-col justify-center text-center bg-gray-300 border border-gray-400 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <h1 class="text-3xl md:text-5xl leading-10 md:leading-30 font-bold text-gray-900 ">
             باقة واحد <br/> بدون تعقيد! 
         </h1>
