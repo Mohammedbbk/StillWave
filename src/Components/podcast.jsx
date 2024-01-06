76feb4f7e3f5c79e9eb2eac6fe9cb85cf02438f8
mohabash011 <mohabashuni@gmail.com>
2024-01-06 09:44:33 +0300

    🌈 style(the services & podcast): cards & hover

--- src/Components/podcast.jsx
+++ src/Components/podcast.jsx
@@ -3,48 +3,97 @@ import '../../src/App.css'
 
 export default function Services() {
   return (
-    <div>
-      <div className="bg-gray-400 font-29  flex justify-end"> 
-        <div className="text-right md:pr-32 w-full max-w-7xl"> 
-          <h1 className="text-4xl pr-10 font-bold tracking-tight py-4 text-gray-900 sm:text-5xl md:text-6xl">
+  <div>
+<div class="bg-gray-400 flex font-29 justify-center items-center text-right max-w-7xl">
+    <div style={{ direction: 'rtl' }} class="flex w-3/4 mx-auto flex-col p-4 m-2 justify-center text-center bg-gray-300 border border-gray-400 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h1 class="mb-2 sm:text-3xl md:text-5xl font-bold text-gray-900 dark:text-white">
             ليش لازم تسوّي بودكاست؟ 
-          </h1>
-          <h1 style={{ direction: 'rtl' }} className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
-            يمكنك من الوصول لجمهور محدود العدد، لكنه بالغ التأثير <span className='text-sm md:text-lg'>*حسب الإحصاءات، 90% من المستمعين يُكمِلون الحلقة كاملة!</span>
-          </h1>
-          <h1 className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
-            بوسعك إقناعه بشكل أسهل من غيره في
-            <span className='text-2xl md:text-3xl '> الإيمان بأفكارك </span>
-            أو 
-            <span className='text-2xl md:text-3xl '> اقتناء منتجاتك</span>
-          </h1>
-          <h1 className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
-            ،على عكس منصات التواصل الأخرى، لا يشترط امتلاك شهادة موثوق للإعلان داخل الحلقات
-          </h1>
-          <h1 style={{ direction: 'rtl' }} className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
+        </h1>
+        <ul style={{ direction: 'rtl' }} className='list-disc list-inside'>
+        <li class="text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+            يمكنك من الوصول لجمهور محدود العدد، لكنه بالغ التأثير <span class='text-sm md:text-lg'>*حسب الإحصاءات، 90% من المستمعين يُكمِلون الحلقة كاملة!</span>
+        </li>
+        <li class="text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+            بوسعك إقناعه بشكل أسهل من غيره في <span class='font-bold'> الإيمان بأفكارك </span> أو <span class='font-bold'> اقتناء منتجات.</span>
+        </li>
+        <li class="text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
+         على عكس منصات التواصل الأخرى، لا يشترط امتلاك شهادة موثوق للإعلان داخل الحلقات.
+        </li>
+        <li class="text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
             ولست ملزمًا بالإفصاح عن الإعلان. 
-          </h1>
-          <h1 style={{ direction: 'rtl' }} className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
+        </li>
+        <li class="text-xl sm:text-xl md:text-2xl pr-8 tracking-tight py-4 text-gray-900 dark:text-gray-400">
             وهذا يمكنك من جلب إيرادات لك بدون أي تكاليف إضافية.
-          </h1>
-        </div>
-      </div>
+        </li>
+        </ul>
+    </div>
+    </div>
 
-      <div className="bg-gray-400 font-29 pt-36 flex justify-end"> 
-        <div className="text-right md:pr-32 w-full max-w-7xl"> 
-          <h1 className="text-4xl pr-10 font-bold tracking-tight py-4 text-gray-900 sm:text-5xl md:text-6xl">
+    <div class="bg-gray-400 flex justify-center items-center font-29 text-right max-w-7xl">
+    <div style={{ direction: 'rtl' }} class="flex w-3/4 mx-auto flex-col p-4 m-2 justify-center text-center bg-gray-300 border border-gray-400 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h1 class="mb-2 sm:text-3xl md:text-5xl font-bold text-gray-900 dark:text-white">
             باقة واحدة <br/> بدون تعقيد 
-          </h1>
-          <h1 style={{ direction: 'rtl' }}  className="text-xl pr-10 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28">
+        </h1>
+        <h1 class="text-xl sm:text-xl md:text-2xl pr-10 tracking-tight py-4 text-gray-900 dark:text-gray-400">
             باشتراك واحد، مرن، غير محدود المدة.
-          </h1>
-          <h1 style={{ direction: 'rtl' }}  className="text-xl pr-10 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28">
+        </h1>
+        <h1 class="text-xl sm:text-xl md:text-2xl pr-10 tracking-tight py-4 text-gray-900 dark:text-gray-400">
             تشمل الباقة إنتاج 8 حلقات بودكاست، <br/>
             بغض النظر عن مدة الحلقة،<br/>
-           وبمدة لامحدودة للاشتراك.<br/>
-          </h1>
-        </div>
-      </div> 
+            وبمدة لامحدودة للاشتراك.<br/>
+        </h1>
     </div>
+</div>
+
+
+    
+</div>
+
+
+    // <div>
+    //   <div className="bg-gray-400 font-29  flex justify-end"> 
+    //     <div className="text-right md:pr-32 w-full max-w-7xl"> 
+    //       <h1 className="text-4xl pr-10 font-bold tracking-tight py-4 text-gray-900 sm:text-5xl md:text-6xl">
+    //         ليش لازم تسوّي بودكاست؟ 
+    //       </h1>
+    //       <h1 style={{ direction: 'rtl' }} className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
+    //         يمكنك من الوصول لجمهور محدود العدد، لكنه بالغ التأثير <span className='text-sm md:text-lg'>*حسب الإحصاءات، 90% من المستمعين يُكمِلون الحلقة كاملة!</span>
+    //       </h1>
+    //       <h1 className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
+    //         بوسعك إقناعه بشكل أسهل من غيره في
+    //         <span className='text-2xl md:text-3xl '> الإيمان بأفكارك </span>
+    //         أو 
+    //         <span className='text-2xl md:text-3xl '> اقتناء منتجاتك</span>
+    //       </h1>
+    //       <h1 className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
+    //         ،على عكس منصات التواصل الأخرى، لا يشترط امتلاك شهادة موثوق للإعلان داخل الحلقات
+    //       </h1>
+    //       <h1 style={{ direction: 'rtl' }} className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
+    //         ولست ملزمًا بالإفصاح عن الإعلان. 
+    //       </h1>
+    //       <h1 style={{ direction: 'rtl' }} className="text-xl pr-8 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28"> 
+    //         وهذا يمكنك من جلب إيرادات لك بدون أي تكاليف إضافية.
+    //       </h1>
+    //     </div>
+    //   </div>
+
+
+
+    //   <div className="bg-gray-400 font-29 pt-36 flex justify-end"> 
+    //     <div className="text-right md:pr-32 w-full max-w-7xl"> 
+    //       <h1 className="text-4xl pr-10 font-bold tracking-tight py-4 text-gray-900 sm:text-5xl md:text-6xl">
+    //         باقة واحدة <br/> بدون تعقيد 
+    //       </h1>
+    //       <h1 style={{ direction: 'rtl' }}  className="text-xl pr-10 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28">
+    //         باشتراك واحد، مرن، غير محدود المدة.
+    //       </h1>
+    //       <h1 style={{ direction: 'rtl' }}  className="text-xl pr-10 tracking-tight py-4 text-gray-900 md:text-2xl md:pr-28">
+    //         تشمل الباقة إنتاج 8 حلقات بودكاست، <br/>
+    //         بغض النظر عن مدة الحلقة،<br/>
+    //        وبمدة لامحدودة للاشتراك.<br/>
+    //       </h1>
+    //     </div>
+    //   </div> 
+    // </div>
   );
 }
--- src/Components/services.jsx
+++ src/Components/services.jsx
@@ -3,8 +3,8 @@ import '../../src/App.css'
 
 export default function Services() {
   return (
-    <div id="Services" className="bg-gray-400 font-29 pb-64 flex justify-end"> 
-      <div className="text-right md:pr-32 w-full max-w-7xl"> 
+    <div id="Services" className="bg-gray-400 font-29 pb-64 flex justify-center text-center text-right  max-w-7xl "> 
+      {/* <div className="text-right md:pr-32 w-full max-w-7xl"> 
         <h1 style={{ direction: 'rtl' }} className="text-4xl pr-10 font-bold tracking-tight py-4 text-gray-900 sm:text-5xl md:text-6xl">
           أفضل شريك لك في إنتاج<br/>برامجك الصوتية:
         </h1>
@@ -12,7 +12,7 @@ export default function Services() {
         سواء كان برنامجك جاهزًا أم غير ذلك، نساعدك في
         </h2>
         
-        <div className="flex text-xl font-bold leading-8 p-5 sm:text-2xl md:text-3xl text-gray-900 flex-wrap gap-10 mt-6 justify-center md:justify-start">
+        <div className="flex text-xl font-bold leading-8 p-5 sm:text-2xl md:text-3xl text-gray-900 flex-wrap gap-10 mt-6 justify-center">
           <p className=" ">
             تحـريــر <br/> الصوت
           </p>
@@ -29,7 +29,31 @@ export default function Services() {
             دعم واستشارات <br/>غير محـــــــدودة
           </p>
         </div>
-      </div>
+      </div> */}
+
+<div style={{ direction: 'rtl' }} class="transform transition duration-400 hover:scale-105 flex w-3/4 mx-auto flex-col p-4 m-2 justify-center text-center bg-gray-300 border border-gray-400 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <h5 class="mb-2 w-full sm:text-3xl md:text-5xl  font-bold text-gray-900 dark:text-white">أفضل شريك لك في إنتاج<br/>برامجك الصوتية: </h5>
+    <p class="mb-5 w-full text-base text-gray-900 sm:text-xl text-2xl dark:text-gray-400">سواء كان برنامجك جاهزًا أم غير ذلك، نساعدك في:</p>
+    <div class="flex flex-row text-xl font-bold leading-8 p-5 sm:text-2xl md:text-3xl text-gray-900 flex-wrap gap-10 mt-6 justify-center">
+      <p class="flex justify-center item-center">
+        تحـريــر <br/> الصوت
+      </p>
+      <p class="text-center">
+        تصميم<br/> الكوفرز
+      </p>
+      <p class="text-center">
+        النشــــــر  <br/>والتوزيع
+      </p>
+      <p class="text-center">
+        كتابة المحتوى <br/>التســـويــقــي
+      </p>
+      <p class="text-center">
+        دعم واستشارات <br/>غير محـــــــدودة
+      </p>
     </div>
+</div>
+</div>
+    
   );
 }
+
